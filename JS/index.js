a27cb2f27d7af583596fb422763a34a683931d2f
andresfelta95 <andresfelta95@gmail.com>
2022-06-07 11:00:39 -0600

    fixing a display bug in JS

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AQE123", new Account("Andres Camelo", "1A543R357P"));
 car.passanger = 4;
 car.printDataCar();
+
+var uberX = new UberX("AWD168", new Account("Benito Camelas", "NASE186"), "Honda", "Civic");
+uberX.passanger = 4;
+uberX.printDataCar();
